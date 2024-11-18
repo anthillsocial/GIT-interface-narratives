e7cb4bf77ae85ec13cdd8640258dcba390e2ef1d
Tom Keene <tom@theanthillsocial.co.uk>
2024-11-18 12:25:35 +0000

    Added commandline options

---
+++ examples/popup-windows/openApps.sh
@@ -0,0 +1,11 @@
+# COMMANDS TO AUTOMATICALY OPEN WINDOWS
+
+# USING THE COMMANDLINE ON MAC OSx
+man open      # Instructions for the 'open command'
+open ~/Desktop  # Open the Desktop folder
+open https://theanthillsocial.co.uk # Open a URL
+echo "Hello" | open -f # Open TestEdit with this text in a file
+ls | open -f  # List contents of the current directory
+open *        # Open all files in the current directory
+
+# Run a sequence of these commands in a bash script!
